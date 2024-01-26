f6d82c93c90195bb69c732dc03ecccb977cb2c7c
borabey <bora.b.sarikaya@gmail.com>
2024-01-26 09:49:29 +0200

    wrap app with BrowserRouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css'
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 function App() {
-
   return (
     <>
+      <header></header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+      <footer></footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
