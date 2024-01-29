80ef6fc1bf97fbc8ce72ef4243fc9073e4b0dff9
borabey <bora.b.sarikaya@gmail.com>
2024-01-29 17:19:57 +0200

    tidy up index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,27 @@
+require('dotenv/config');
 const express = require("express");
-const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
-dotenv.config();
-connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+
+// Routes
+app.use("/users", userRouter);
+
+// Database connection
+connectDB()
+  .then(() => {
+    // Start the server
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => {
+      console.log(`Server is up on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+  });
