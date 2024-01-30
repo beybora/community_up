94fda035a9ca4a7ce7d73729b7e2a50ec7e17a07
borabey <bora.b.sarikaya@gmail.com>
2024-01-30 19:13:36 +0200

    correct typo, add new route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -8,11 +8,12 @@ router.post("/login", userController.login);
 router.post("/logout", userController.logout);
 router.post("/currentUser", authenticate, userController.getLoggedUser);
 router.get("/", userController.getAllUsers);
-// router.get("/search",authenticate, userController.searchUser);
+router.get("/search", userController.searchUser);
 router.get("/:id", userController.getUserById);
 router.put("/:id", userController.updateUserById);
 router.delete("/:id", userController.deleteUserById);
 router.get("/community/:communityId/users", userController.getUsersInCommunity);
-router.get("/communit/:eventId/users", userController.getUsersInEvent);
+router.get("/event/:eventId/users", userController.getUsersInEvent);
+
 
 module.exports = router;
