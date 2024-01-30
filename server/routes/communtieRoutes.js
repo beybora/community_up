cd7f6e2268ccf42e6187a1125a6bc6d6aa7e1c94
borabey <bora.b.sarikaya@gmail.com>
2024-01-30 17:48:44 +0200

    add routes for communites

---
+++ server/routes/communtieRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const communityController = require("../controllers/communityController");
+const authenticate = require("../middleware/auth");
+
+// Community routes
+router.get("/place/:placeId", communityController.getCommunitiesByPlace);
+//middleware to request authentication for using routes below
+router.use(authenticate);
+router.post("/create", communityController.createCommunity);
+router.get("/", communityController.getAllCommunities);
+router.get("/:id", communityController.getCommunityById);
+router.put("/:id", communityController.updateCommunityById);
+router.delete("/:id", communityController.deleteCommunityById);
+
+module.exports = router;
