5510ea3abb65e23c912192a655171017e017f66f
borabey <bora.b.sarikaya@gmail.com>
2024-01-30 14:04:15 +0200

    add Login and Register to HomePage

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,5 +1,26 @@
+import React, { useState } from 'react';
+import Login from '../../components/LogIn';
+import Register from '../../components/Register';
+
+
 const Home = () => {
-  return <></>;
+  const [isLoginTab, setLoginTab] = useState(true);
+
+  const handleTabChange = () => {
+    setLoginTab(!isLoginTab);
+  };
+
+  return (
+    <>
+      <div className="tab-container">
+        <button onClick={handleTabChange}>
+          {isLoginTab ? 'Switch to Register' : 'Switch to Login'}
+        </button>
+      </div>
+
+      {isLoginTab ? <Login /> : <Register />} 
+    </>
+  );
 };
 
 export default Home;
