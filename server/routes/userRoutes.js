e77a8dc82b1379f23d6b28639293bd115a8411c3
borabey <bora.b.sarikaya@gmail.com>
2024-01-31 12:09:29 +0200

    correct request type

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,7 +6,7 @@ const authenticate = require("../middleware/auth");
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
-router.post("/currentUser", authenticate, userController.getLoggedUser);
+router.get("/currentUser", authenticate, userController.getLoggedUser);
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getUserById);
 router.put("/:id", userController.updateUserById);
