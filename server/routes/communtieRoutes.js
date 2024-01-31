eb378984a726feaa598d7ae231ca43702990f216
borabey <bora.b.sarikaya@gmail.com>
2024-01-31 15:47:54 +0200

    comment out authentication

--- server/routes/communtieRoutes.js
+++ server/routes/communtieRoutes.js
@@ -6,7 +6,7 @@ const authenticate = require("../middleware/auth");
 // Community routes
 router.get("/place/:placeId", communityController.getCommunitiesByPlace);
 //middleware to request authentication for using routes below
-router.use(authenticate);
+//router.use(authenticate);
 router.post("/create", communityController.createCommunity);
 router.get("/", communityController.getAllCommunities);
 router.get("/:id", communityController.getCommunityById);
