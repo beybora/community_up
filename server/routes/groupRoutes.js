5821c520026c1cc0c767791b54b7a0f563f1d2d7
borabey <bora.b.sarikaya@gmail.com>
2024-02-01 09:33:05 +0200

    add new controller for "joing" adding user to group

--- server/routes/groupRoutes.js
+++ server/routes/groupRoutes.js
@@ -5,11 +5,12 @@ const groupController = require("../controllers/groupController");
 // Define routes
 router.get("/groups", groupController.getAllGroups);
 router.get("/groups/:id", groupController.getGroupById);
-router.post("/groups", groupController.createGroup);
+router.post("/groups", groupController.createGroupAndAddToCommunity);
 router.put("/groups/:id", groupController.updateGroupById);
 router.delete("/groups/:id", groupController.deleteGroupById);
 router.get("/public-groups", groupController.getPublicGroups);
 router.get("/groups-by-community/:communityId", groupController.getGroupsByCommunity);
 router.get("/groups-by-member/:memberId", groupController.getGroupsByMember);
+router.post("/join-group", groupController.joinGroup);
 
 module.exports = router;
