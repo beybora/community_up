19c332c481b6bc123a283fc17acdeecd798aa014
borabey <bora.b.sarikaya@gmail.com>
2024-02-02 16:08:33 +0200

    add new routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,17 +7,17 @@ const authenticate = require("../middleware/auth");
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
-//router.use(authenticate);
+router.use(authenticate);
 router.get("/currentUser", userController.getLoggedUser);
 router.get("/", userController.getAllUsers);
 //router.get("/search", userController.searchUser);
-router.get("/:id", userController.getUserById);
+//router.get("/:id", userController.getUserById);
 router.put("/:id", userController.updateUserById);
-router.delete("/:id", userController.deleteUserById);
+//router.delete("/:id", userController.deleteUserById);
 router.get("/community/:communityId/users", userController.getUsersInCommunity);
 router.get("/event/:eventId/users", userController.getUsersInEvent);
-router.get('/user/joined-communities', userController.getJoinedCommunities);
-router.get('/user/joined-groups/:communityId', userController.getJoinedGroups);
-
+router.get('/joined-communities', userController.getJoinedCommunities);
+router.get('/joined-groups/:communityId', userController.getJoinedGroups);
+router.get("/all-groups", userController.getAllUserGroups);
 
 module.exports = router;
