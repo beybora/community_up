327e46b95f7cd0873ddd2b596fa7fced59848c63
borabey <bora.b.sarikaya@gmail.com>
2024-02-02 12:58:02 +0200

    add route to login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,21 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import HomePage from "./pages/HomePage";
+import Login from "./components/LogIn";
 function App() {
   return (
     <div className="App">
       <header></header>
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </main>
       <footer></footer>
     </div>
   );
 }
+
 
 export default App;
