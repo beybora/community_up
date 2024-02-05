e63391a4e3d2a213aba22855cf97d59116ca3a2a
borabey <bora.b.sarikaya@gmail.com>
2024-02-05 17:44:59 +0200

    adjust authentication component

--- client/src/pages/Authentication/index.jsx
+++ client/src/pages/Authentication/index.jsx
@@ -3,24 +3,56 @@ import axios from "../../axiosinstance";
 import React, { useState } from "react";
 import Login from "../../components/LogIn";
 import Register from "../../components/Register";
-
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
 const Authentication = () => {
   const [isLoginTab, setLoginTab] = useState(true);
-    
+
   const handleTabChange = () => {
     setLoginTab(!isLoginTab);
   };
 
   return (
-    <>
-      <div className="tab-container">
-        <button onClick={handleTabChange}>
-          {isLoginTab ? "Switch to Register" : "Switch to Login"}
-        </button>
-      </div>
-
-      {isLoginTab ? <Login /> : <Register />}
-    </>
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" justifyContent="center" display="flex" fontFamily="fantasy">
+          COMMUNITY UP
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab>Login</Tab>
+            <Tab>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Register />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
   );
 };
 
