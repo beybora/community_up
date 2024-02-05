776b06a50880b63ce19f2b53c3d05d06a67fcf4b
borabey <bora.b.sarikaya@gmail.com>
2024-02-05 17:46:25 +0200

    add new Routes of newly added pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,26 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Login from "./components/LogIn";
+import ChatPage from "./pages/ChatPage";
+import Authentication from "./pages/Authentication";
+import Communities from "./components/Communities";
+import CommunitesPage from "./pages/CommunitiesPage";
+
+
 function App() {
   return (
     <div className="App">
       <header></header>
       <main>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/community/:id" element={<ChatPage />} />
+          <Route path="/login" element={<Authentication />} />
+          <Route path="/communities" element={<CommunitesPage />} />
         </Routes>
       </main>
       <footer></footer>
     </div>
   );
 }
-
 
 export default App;
