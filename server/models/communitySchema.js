9b189d7f364dfab00097cc9839badbd0646e4d2a
borabey <bora.b.sarikaya@gmail.com>
2024-02-12 15:12:52 +0200

    change field name

--- server/models/communitySchema.js
+++ server/models/communitySchema.js
@@ -7,7 +7,7 @@ const communitySchema = new mongoose.Schema(
     isPrivate: { type: Boolean, default: false },
     location: { type: mongoose.Schema.Types.ObjectId, ref: "Place" },
     members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    moderators: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    admins: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     groups: [{ type: mongoose.Schema.Types.ObjectId, ref: "Group" }],
     events: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
   },
